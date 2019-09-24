f3eb51b731945e1c622bcbeaf707b4e68c13b102
Olivier Giroux <ogiroux@gmail.com>
2019-09-24 16:43:11 -0700

    Now with a benchmark that works with CUDA

--- samples/benchmark.cpp
+++ samples/benchmark.cpp
@@ -57,15 +57,64 @@ THE SOFTWARE.
 //#include <latch>
 //#include <barrier>
 
+#ifdef __NVCC__
+# define _ABI __host__ __device__
+# define check(ans) { assert_((ans), __FILE__, __LINE__); }
+inline void assert_(cudaError_t code, const char *file, int line) {
+   if (code == cudaSuccess) return;
+    std::cerr << "check failed: " << cudaGetErrorString(code) << " : " << file << '@' << line << std::endl;
+    abort();
+}
+#else
+# define _ABI
+#endif
+
+template <class T>
+struct managed_allocator {
+  typedef simt::std::size_t size_type;
+  typedef simt::std::ptrdiff_t difference_type;
+
+  typedef T value_type;
+  typedef T* pointer;// (deprecated in C++17)(removed in C++20)	T*
+  typedef const T* const_pointer;// (deprecated in C++17)(removed in C++20)	const T*
+  typedef T& reference;// (deprecated in C++17)(removed in C++20)	T&
+  typedef const T& const_reference;// (deprecated in C++17)(removed in C++20)	const T&
+
+  template< class U > struct rebind { typedef managed_allocator<U> other; };
+  managed_allocator() = default;
+  template <class U> constexpr managed_allocator(const managed_allocator<U>&) noexcept {}
+  T* allocate(std::size_t n) {
+    void* out = nullptr;
+#ifdef __NVCC__
+    check(cudaMallocManaged(&out, n*sizeof(T)));
+#else
+    out = malloc(n*sizeof(T));
+#endif
+    return static_cast<T*>(out);
+  }
+  void deallocate(T* p, std::size_t) noexcept { 
+#ifdef __NVCC__
+    check(cudaFree(p)); 
+#else
+    free(p);
+#endif
+  }
+};
+template<class T, class... Args>
+T* make_(Args &&... args) {
+    managed_allocator<T> ma;
+    return new (ma.allocate(1)) T(std::forward<Args>(args)...);
+}
+
 struct mutex {
-	void lock() noexcept {
+	_ABI void lock() noexcept {
 		while (1 == l.exchange(1, simt::std::memory_order_acquire))
 #ifndef __NO_WAIT
 			l.wait(1, simt::std::memory_order_relaxed)
 #endif
             ;
 	}
-	void unlock() noexcept {
+	_ABI void unlock() noexcept {
 		l.store(0, simt::std::memory_order_release);
 #ifndef __NO_WAIT
 		l.notify_one();
@@ -75,7 +124,7 @@ struct mutex {
 };
 
 struct ticket_mutex {
-	void lock() noexcept {
+	_ABI void lock() noexcept {
         auto const my = in.fetch_add(1, simt::std::memory_order_acquire);
         while(1) {
             auto const now = out.load(simt::std::memory_order_acquire);
@@ -86,7 +135,7 @@ struct ticket_mutex {
 #endif
         }
 	}
-	void unlock() noexcept {
+	_ABI void unlock() noexcept {
 		out.fetch_add(1, simt::std::memory_order_release);
 #ifndef __NO_WAIT
 		out.notify_all();
@@ -125,18 +174,31 @@ sum_mean_dev_t sum_mean_dev(V && v) {
     return std::tie(sum, mean, stddev);
 }
 
-template <class F>
-sum_mean_dev_t test_body(int threads, F && f) {
+#ifdef __NVCC__
+template<class F>
+__global__ void launcher(F f, int s_per_t, int* p) {
+    p[blockIdx.x * blockDim.x + threadIdx.x] = (*f)(s_per_t);
+}
+#endif
 
-    std::vector<int> progress(threads, 0);
+template <class F>
+sum_mean_dev_t test_body(int threads, F f) {
+
+    std::vector<int, managed_allocator<int>> progress(threads, 0);
+
+#ifdef __NVCC__
+    auto f_ = make_<F>(f);
+    launcher<<<threads, 1>>>(f_, sections / threads, &progress[0]);
+    cudaDeviceSynchronize();
+#else
 	std::vector<std::thread> ts(threads);
 	for (int i = 0; i < threads; ++i)
 		ts[i] = std::thread([&, i]() {
             progress[i] = f(sections / threads);
         });
-
 	for (auto& t : ts)
 		t.join();
+#endif
 
     return sum_mean_dev(progress);
 }
@@ -156,11 +218,11 @@ sum_mean_dev_t test_omp_body(int threads, F && f) {
 }
 
 template <class F>
-void test(std::string const& name, int threads, F && f, std::atomic<bool>& keep_going, bool use_omp = false) {
+void test(std::string const& name, int threads, F && f, simt::std::atomic<bool>& keep_going, bool use_omp = false) {
 
     std::thread test_helper([&]() {
         std::this_thread::sleep_for(std::chrono::seconds(2));
-        keep_going.store(false, std::memory_order_relaxed);
+        keep_going.store(false, simt::std::memory_order_relaxed);
     });
 
     auto const t1 = std::chrono::steady_clock::now();
@@ -203,31 +265,31 @@ void test_loop(F && f) {
 
 template<class M>
 void test_mutex(std::string const& name, bool use_omp = false) {
-    test_loop([&](auto c) {
-        M m;
-        std::atomic<bool> keep_going(true);
-        auto f = [&](int n) -> int {
+    test_loop([&](std::pair<int, std::string> c) {
+        M* m = make_<M>();
+        simt::std::atomic<bool> *keep_going = make_<simt::std::atomic<bool>>(true);
+        auto f = [=] _ABI (int n) -> int {
             int i = 0;
-            while(keep_going.load(std::memory_order_relaxed)) {
-                m.lock();
+            while(keep_going->load(simt::std::memory_order_relaxed)) {
+                m->lock();
                 ++i;
-                m.unlock();
+                m->unlock();
             }
             return i;
         };
-        test(name + ": " + c.second, c.first, f, keep_going);
+        test(name + ": " + c.second, c.first, f, *keep_going);
     });
 };
 
 template<class B>
 void test_barrier(std::string const& name, bool use_omp = false) {
 
-    test_loop([&](auto c) {
-        B b(c.first);
-        std::atomic<bool> keep_going(true); // unused here
-        auto f = [&](int n)  -> int {
+    test_loop([&](std::pair<int, std::string> c) {
+        B* b = make_<B>(c.first);
+        simt::std::atomic<bool> *keep_going = make_<simt::std::atomic<bool>>(true);
+        auto f = [=] _ABI (int n)  -> int {
             for (int i = 0; i < n; ++i)
-                b.arrive_and_wait();
+                b->arrive_and_wait();
             return n;
         };
         test(name + ": " + c.second, c.first, f, keep_going, use_omp);
