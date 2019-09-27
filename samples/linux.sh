153faf1929e88dc50f46f6ab9fc8eae40c38eba7
Olivier Giroux <ogiroux@gmail.com>
2019-09-27 12:38:37 -0700

    Pre full barrier fix

--- samples/linux.sh
+++ samples/linux.sh
@@ -2,5 +2,4 @@ g++ -std=c++11 trie_st.cpp -O2 -o trie_st
 g++ -std=c++11 trie_mt.cpp -O2 -o trie_mt -pthread
 nvcc -I../include -arch=compute_70 -std=c++11 -O2 trie.cu --expt-relaxed-constexpr -o trie
 g++ -I../include -std=c++14 benchmark.cpp -O2 -lpthread -o benchmark
-g++ -I../include -D_LIBCPP_BARRIER_BUTTERFLY -std=c++14 benchmark.cpp ../libcxx/src/barrier.cpp -O2 -lpthread -o benchmark
 nvcc -I../include -arch=compute_70 -std=c++14 benchmark.cu -O2 -lpthread --expt-relaxed-constexpr --expt-extended-lambda -o benchmark
