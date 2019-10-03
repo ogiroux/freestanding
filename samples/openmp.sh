5654f2bcc8972d513cf9cedf28510c1b6613aeeb
Olivier Giroux <ogiroux@gmail.com>
2019-10-02 22:00:53 -0700

    Fixes on power.

--- samples/openmp.sh
+++ samples/openmp.sh
@@ -1,1 +1,1 @@
-clang++ -D_LIBCPP_BARRIER_BUTTERFLY -I../include -fopenmp=libomp -L../../llvm-project/build/lib/ -std=c++11 -O2 benchmark.cpp -lstdc++ -lpthread -lm -o benchmark
+clang++ -D_LIBCPP_BARRIER_BUTTERFLY -I../include -I/usr/local/cuda/include -fopenmp=libomp -L../../llvm-project/build/lib/ -std=c++11 -O2 benchmark.cpp -lstdc++ -lpthread -lm -o benchmark
